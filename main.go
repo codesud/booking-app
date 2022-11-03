b0144e7f1cc63befc4eb95bad45bc700a1f5f0ce
Sudeep S <sudeepdirect@gmail.com>
2022-11-03 14:33:22 +0530

    Updated go files

--- main.go
+++ main.go
@@ -5,11 +5,10 @@ import "fmt"
 func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 50
-
+	var remainingTickets = 50
 	fmt.Println("Welcome to our", conferenceName, "booking application")
+	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available" )
 	fmt.Println("Get your tickets here to attend")
 
-	conferenceTickets = 30
-	fmt.Println(conferenceTickets)
 	
 }
