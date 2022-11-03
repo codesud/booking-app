de8fae338ee23d38470a62b90d87a22b935b36da
Sudeep S <sudeepdirect@gmail.com>
2022-11-03 19:55:38 +0530

    Updated go files

--- main.go
+++ main.go
@@ -70,6 +70,6 @@ func main() {
 
 func greetUsers(confName string, confTickets int, remainTickets uint) {
 	fmt.Printf("Welcome to %v booking application.\n", confName)
-	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", confTickets, remainTickets)
 	fmt.Println("Get your tickets here to attend.")
 }
