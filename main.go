c0108f73515d1d072ada94f85a1ec1df8730444b
Sudeep S <sudeepdirect@gmail.com>
2022-11-03 14:00:23 +0530

    Updated go files

--- main.go
+++ main.go
@@ -4,7 +4,8 @@ import "fmt"
 
 func main() {
 	var conferenceName = "Go Conference"
-	fmt.Println("Welcome to our" conferenceName "booking application")
+
+	fmt.Println("Welcome to our", conferenceName, "booking application")
 	fmt.Println("Get your tickets here to attend")
 
 }
