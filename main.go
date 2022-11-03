c9a8ce5349affd56247cb231ff7ea4e34deec96a
Sudeep S <sudeepdirect@gmail.com>
2022-11-03 14:36:30 +0530

    Updated go files

--- main.go
+++ main.go
@@ -6,9 +6,9 @@ func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 50
 	var remainingTickets = 50
-	fmt.Println("Welcome to our", conferenceName, "booking application")
-	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available" )
-	fmt.Println("Get your tickets here to attend")
+	fmt.Println("Welcome to our", conferenceName, "booking application.")
+	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available." )
+	fmt.Println("Get your tickets here to attend.")
 
 	
 }
